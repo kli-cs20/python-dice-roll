16d29c4248e787c2ff4f091428222d34242ec015
kendallli587 <kendallli587@gmail.com>
2022-07-22 12:54:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 import random
 
 n = 1
-num = float(input("How many times would you like to roll? "))
+numRolls = float(input("How many times would you like to roll? "))
 
-while n <= num:
+while n <= numRolls:
     # Roll a Dice
     randNum = random.randrange(1, 7)
     print(randNum)
